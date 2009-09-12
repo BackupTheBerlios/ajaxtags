e927964490ebf0cc40a3f0414ec2de145984c8eb
jenskapitza <j.kapitza@schwarze-allianz.de>
2009-09-12 19:28:53 +0200

    license update

--- ajaxtags-resources/src/main/resources/net/sourceforge/ajaxtags/js/ajaxtags.js
+++ ajaxtags-resources/src/main/resources/net/sourceforge/ajaxtags/js/ajaxtags.js
@@ -1,19 +1,19 @@
 /**
- * Copyright 2009 Jens Kapitza
+ * Copyright 2009 AjaxTags-Team
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 /*jslint bitwise: true, eqeqeq: true, immed: true, newcap: true, nomen: true, regexp: true, undef: true, white: true, maxerr: 50, indent: 4 */
 /*global $,$$,$F,Ajax,Autocompleter,Class,Element,Event,Form,Option,Prototype,overlib*/
 
