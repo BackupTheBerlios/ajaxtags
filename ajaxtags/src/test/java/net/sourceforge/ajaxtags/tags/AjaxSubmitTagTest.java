ea24ac99bee186dbfeafa7fd979aadd5a317794c
V.Homyakov <devnull@localhost>
2009-10-05 14:07:53 +0300

    Common tag testing methods are moved into new class AbstractTagTest

--- ajaxtags/src/test/java/net/sourceforge/ajaxtags/tags/AjaxSubmitTagTest.java
+++ ajaxtags/src/test/java/net/sourceforge/ajaxtags/tags/AjaxSubmitTagTest.java
@@ -16,19 +16,13 @@
  */
 package net.sourceforge.ajaxtags.tags;
 
-import static org.junit.Assert.assertEquals;
-
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
-import javax.servlet.jsp.tagext.BodyTag;
-import javax.servlet.jsp.tagext.Tag;
 import javax.xml.transform.TransformerException;
 
 import net.sourceforge.ajaxtags.FakeBodyContent;
 import net.sourceforge.ajaxtags.FakePageContext;
-import net.sourceforge.ajaxtags.helpers.XMLUtils;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXException;
@@ -37,28 +31,18 @@ import org.xml.sax.SAXException;
  * Test for AjaxSubmitTag.
  *
  * @author Victor Homyakov
- * @version $Revision$ $Date$ $Author$
  */
-public class AjaxSubmitTagTest {
-
-    private AjaxSubmitTag tag;
+public class AjaxSubmitTagTest extends AbstractTagTest<AjaxSubmitTag> {
 
     /**
      * Set up.
+     *
+     * @throws Exception
+     *             if setUp fails
      */
     @Before
-    public void setUp() {
-        tag = new AjaxSubmitTag();
-        tag.setBodyContent(new FakeBodyContent());
-        tag.setPageContext(new FakePageContext());
-    }
-
-    /**
-     * Tear down.
-     */
-    @After
-    public void tearDown() {
-        tag.release();
+    public void setUp() throws Exception {
+        setUp(AjaxSubmitTag.class);
     }
 
     /**
@@ -78,21 +62,15 @@ public class AjaxSubmitTagTest {
         tag.setSource("formId");
         tag.setTarget("targetId");
 
-        assertEquals("doStartTag() must return Tag.SKIP_BODY", Tag.SKIP_BODY, tag.doStartTag());
-        assertEquals("doAfterBody() must return BodyTag.SKIP_BODY", BodyTag.SKIP_BODY, tag
-                .doAfterBody());
-        assertEquals("doEndTag() must return BodyTag.EVAL_PAGE", BodyTag.EVAL_PAGE, tag.doEndTag());
+        assertStartTagSkipBody();
+        assertAfterBody();
+        assertEndTag();
 
         final String content = ((FakeBodyContent) context.getOut()).getString();
         final String expected = "<script type=\"text/javascript\">"
                 + "new AjaxJspTag.Submit({source: \"formId\", target: \"targetId\"});"
                 + "</script>";
-
-        assertEquals("HTML after doEndTag()", reformat(expected), reformat(content));
-    }
-
-    private String reformat(final String html) throws TransformerException, SAXException {
-        return XMLUtils.format(html).replaceAll("[\\s\r\n]", "");
+        assertContent(expected, content);
     }
 
 }
